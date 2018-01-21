3aa37208e09da67e84f7e517bd57f21a9ddb0c88
kogayushi <youshe.old.pleasure@gmail.com>
2018-01-21 22:27:28 +0900

    ユーザ名も変更可能にしたい(Object Oriented)
    generatePasswordPolicyForをコピペしてgenerateUsernamePolicyForを作成

--- src/main/java/com/example/ist/object/oriented/domain/model/policy/PolicyFactory.java
+++ src/main/java/com/example/ist/object/oriented/domain/model/policy/PolicyFactory.java
@@ -12,7 +12,27 @@ import java.util.Set;
 @Component
 public class PolicyFactory {
 
-    @SuppressWarnings("unchecked")
+    public UsernamePolicy generateUsernamePolicyFor(User user) {
+        Set<CredentialPolicy> policies = new LinkedHashSet<>();
+
+        policies.add(new LengthPolicy(8, 20));
+        // Compositeパターン refer -> https://ja.wikipedia.org/wiki/Composite_%E3%83%91%E3%82%BF%E3%83%BC%E3%83%B3
+        policies.add(new CharacterCombinationPolicy());
+
+        policies.add(new NotSameWithCurrentUsernamePolicy(user.getCredentials().getUsername()));
+        policies.add(new NotSameWithCurrentUsernamePolicy(user.getCredentials().getUsername()));
+
+        Person person = user.getPerson();
+
+        policies.add(new NotContainsNamePolicy(person.getFirstName(), person.getLastName()));
+
+        ContactInformation contactInformation = person.getContactInformation();
+        policies.add(new NotSameWithMailAddressPolicy(contactInformation.getMailAddress()));
+        policies.add(new NotContainsTelephoneNumberPolicy(contactInformation.getTelephoneNumber()));
+
+        return new UsernamePolicy(policies);
+    }
+
     public PasswordPolicy generatePasswordPolicyFor(User user) {
         Set<CredentialPolicy> policies = new LinkedHashSet<>();
 
