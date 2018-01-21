3ca04f985767c3cd57b06383ccd2100d1c77a9bb
kogayushi <youshe.old.pleasure@gmail.com>
2018-01-21 22:33:05 +0900

    ユーザ名も変更可能にしたい(Object Oriented)
    changePasswordをコピペしてchangeUsernam`を作成

--- src/main/java/com/example/ist/object/oriented/application/IdentityService.java
+++ src/main/java/com/example/ist/object/oriented/application/IdentityService.java
@@ -1,11 +1,14 @@
 package com.example.ist.object.oriented.application;
 
 import com.example.ist.object.oriented.application.command.ChangePasswordCommand;
+import com.example.ist.object.oriented.application.command.ChangeUsernameCommand;
 import com.example.ist.object.oriented.domain.model.identity.Password;
 import com.example.ist.object.oriented.domain.model.identity.User;
 import com.example.ist.object.oriented.domain.model.identity.UserRepository;
+import com.example.ist.object.oriented.domain.model.identity.Username;
 import com.example.ist.object.oriented.domain.model.policy.PasswordPolicy;
 import com.example.ist.object.oriented.domain.model.policy.PolicyFactory;
+import com.example.ist.object.oriented.domain.model.policy.UsernamePolicy;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,19 @@ import java.util.UUID;
 public class IdentityService {
     private final UserRepository userRepository;
     private final PolicyFactory policyFactory;
+
+    public void changeUsername(ChangeUsernameCommand command) {
+
+        UUID id = command.getId();
+        User user = this.userRepository.userFromId(id);
+
+        UsernamePolicy policy = this.policyFactory.generateUsernamePolicyFor(user);
+        Username username = new Username(command.getNewUsername());
+
+        policy.validate(username);
+
+        userRepository.updateUsername(id, username);
+    }
 
     public void changePassword(ChangePasswordCommand command) {
 
